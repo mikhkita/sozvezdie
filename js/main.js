235553df98568196aaa2935b9c891db761e5baf7
VitekNek <viktor-nekrasov54@mail.ru>
2016-07-19 21:39:53 +0800

    third commit

--- js/main.js
+++ js/main.js
@@ -81,11 +81,42 @@ $(document).ready(function(){
     // };
 
     // var jssor_slider1 = new $JssorSlider$("slider1_container", options);
-
+    $('.video-open img').click(function() {
+      $(this).hide();
+      $('.video-open iframe').attr('src','https://www.youtube.com/embed/rgJx_prPhEA?rel=0&autoplay=1');
+      $('.video-open iframe').fadeIn();
+    });
+    
+    $('#slide_video1 img').click(function() {
+      $(this).hide();
+      $('#slide_video1 iframe').attr('src','https://www.youtube.com/embed/rgJx_prPhEA?rel=0&autoplay=1');
+      $('#slide_video1 iframe').fadeIn();
+    });
+    $('#slide_video2 img').click(function() {
+      $(this).hide();
+      $('#slide_video2 iframe').attr('src','https://www.youtube.com/embed/rgJx_prPhEA?rel=0&autoplay=1');
+      $('#slide_video2 iframe').fadeIn();
+    });
+    $('#slide_video3 img').click(function() {
+      $(this).hide();
+      $('#slide_video3 iframe').attr('src','https://www.youtube.com/embed/rgJx_prPhEA?rel=0&autoplay=1');
+      $('#slide_video3 iframe').fadeIn();
+    });
+    $('#slide_video4 img').click(function() {
+      $(this).hide();
+      $('#slide_video4 iframe').attr('src','https://www.youtube.com/embed/rgJx_prPhEA?rel=0&autoplay=1');
+      $('#slide_video4 iframe').fadeIn();
+    });
+    $('#slide_video5 img').click(function() {
+      $(this).hide();
+      $('#slide_video5 iframe').attr('src','https://www.youtube.com/embed/rgJx_prPhEA?rel=0&autoplay=1');
+      $('#slide_video5 iframe').fadeIn();
+    });
 
     $('.head_slide_h2').slick({
       autoplay: true,
-      speed: 1000,
+      autoplaySpeed: 2000,
+      speed: 500,
       dots: true,
       prevArrow: '<div class="arrow-cont left-arrow"><img class=" arrow" src="img/prev_arrow.png"></div>',
       nextArrow: '<div class="arrow-cont right-arrow"><img class=" arrow" src="img/next_arrow.png"></div>'
@@ -107,4 +138,6 @@ $(document).ready(function(){
       $('.hide_inform').fadeIn();
     });
 
+    pic = new Image();
+    pic.src="img/modal_fon.jpg";
 });
